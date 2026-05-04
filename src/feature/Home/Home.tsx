b89f2c136ee6654def48586e267d50f81ad466e2
Thiago Medeiros <thiagotitico1@gmail.com>
2026-05-04 18:08:08 -0300

    Site

---
+++ src/feature/Home/Home.tsx
@@ -0,0 +1,17 @@
+import HomeSection1 from "./Sections/HomeSection1";
+import HomeSection2 from "./Sections/HomeSection2";
+import HomeSection3 from "./Sections/HomeSection3";
+import HomeSection4 from "./Sections/HomeSection4";
+import HomeSection5 from "./Sections/HomeSection5";
+
+export default function Home() {
+    return(
+        <>
+            <HomeSection1/>
+            <HomeSection2/>
+            <HomeSection3/>
+            <HomeSection4/>
+            <HomeSection5/>
+        </>
+    )
+};
---
+++ src/feature/Home/Sections/HomeSection1.tsx
@@ -0,0 +1,10 @@
+import "./styles/HomeSection1.css";
+import logo from "./assets/home.section1.logo.png";
+
+export default function HomeSection1() {
+    return(
+        <section className="HomeSection1">
+            <img src={logo} alt="NOSS Logo" />
+        </section>
+    )
+};
