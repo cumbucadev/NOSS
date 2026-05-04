96f04f6b5c7ed690752005719eca12e4a6babb1a
Thiago Medeiros <thiagotitico1@gmail.com>
2026-05-04 18:31:18 -0300

    Subindo codigo do site

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Footer from "./feature/Footer/Footer"
+import Header from "./feature/Header/Header"
+import Home from "./feature/Home/Home"
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer/>
+    </>
+  )
+};
